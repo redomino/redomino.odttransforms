6fcb66d2469a2691469c56bd29f3eed132e9d27f
Davide Moro <davide.moro@redomino.com>
2014-02-07 12:41:53 +0100

    added more tests

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -4,7 +4,8 @@ Changelog
 0.4 (unreleased)
 ----------------
 
-- Nothing changed yet.
+- Added more tests
+  [davidemoro]
 
 
 0.3 (2014-02-07)
--- redomino/odttransforms/tests/test_portal.py
+++ redomino/odttransforms/tests/test_portal.py
@@ -8,10 +8,18 @@ class TestPortal(unittest.TestCase):
     layer = REDOMINO_ODTTRANSFORMS_INTEGRATION_TESTING
 
     def test_portal_transforms(self):
-        """ portal transforms correctly registered?"""
+        """ portal transforms correctly registered? """
         from Products.PortalTransforms.interfaces import IPortalTransformsTool
         from zope.component import getUtility
         portal_transforms = getUtility(IPortalTransformsTool)
 
         self.assertTrue('odt_transform' in portal_transforms.objectIds())
+
+    def test_mimetype(self):
+        """ custom mimetype registered? """
+        from zope.component import getUtility
+        from Products.MimetypesRegistry.interfaces import IMimetypesRegistryTool
+        mimetypes_registry = getUtility(IMimetypesRegistryTool)
+        self.assertEquals(1, len(mimetypes_registry.lookup('application/vnd.oasis.opendocument.text.transformed')))
+
 
